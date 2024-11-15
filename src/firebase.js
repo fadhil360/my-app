2bbb13b818a38229b2ed28148eb0cdc885007ef5
Undead <undead.gamer313@gmail.com>
2024-11-15 18:52:48 +0700

    ddd

--- src/firebase.js
+++ src/firebase.js
@@ -1,17 +1,15 @@
-
-// src/firebase.js
 import { initializeApp } from 'firebase/app';
 import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword } from 'firebase/auth';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyA3niHE0kklY7gUFyNtAXNtYaDRENiKHmY",
-    authDomain: "webdemo-3cd29.firebaseapp.com",
-    projectId: "webdemo-3cd29",
-    storageBucket: "webdemo-3cd29.firebasestorage.app",
-    messagingSenderId: "773019625150",
-    appId: "1:773019625150:web:1b57c4775a85e5fc9120ef",
-    measurementId: "G-6LKLY0T5GH"
-  };
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
+};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
